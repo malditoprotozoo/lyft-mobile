8741ec850bb058205a3f313c561d4fedaf69ef1e
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-14 04:52:18 -0300

    Verify phone view added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,66 @@
+var randomNumber = (function() {
+  var code = "";
+  var str = "1234567890";
+  for (var i = 0; i < 3; i++) {
+    code += str.charAt(Math.floor(Math.random() * str.length));
+  }
+  return code;
+});
+
 $(document).ready(function() {
-  $("#country").countrySelect();
   setTimeout(function(){
     $("#loader").fadeOut();
   },5000);
   $("#signup-btn").click(function() {
     $("#login").fadeOut();
+    $("body").append(sectionSignup);
+    $("#country").countrySelect();
+    $("#loader").css({"display":"flex"});
     $("#signup").fadeIn();
-  });
-
-  /**
-  * Determine the calling code per country
-  */
-  $("#phone-input").click(function() {
-    for (var i = 0; i < data.length; i++) {
-      var countryVal = $("#country").val();
-      if (countryVal.includes(data[i].name.common) || countryVal.includes(data[i].name.common)) {
-        $(this).val(data[i].callingCode);
+    setTimeout(function(){
+      $("#loader").fadeOut();
+    },1000);
+    /**
+    * Determine the calling code per country
+    */
+    $("#phone-input").click(function() {
+      for (var i = 0; i < data.length; i++) {
+        var countryVal = $("#country").val();
+        if (countryVal.includes(data[i].name.common) || countryVal.includes(data[i].name.common)) {
+          $(this).val(data[i].callingCode);
+        }
       }
-    }
+    });
+
+    $("#phone-input").keyup(function() {
+      var avoidLetters = /^[0-9]*$/;
+      var thisInput = $(this).val();
+      if (thisInput.length >= 10 && avoidLetters.test(thisInput)) {
+        $("#signup-next-btn").removeClass("disabled");
+      } else {
+        $("#signup-next-btn").addClass("disabled");
+      }
+    });
+
+    $("#signup-next-btn").click(function() {
+      var userCode = randomNumber();
+      alert(userCode);
+      $("#signup").fadeOut();
+      $("#loader").css({"display":"flex"});
+      $("body").append(sectionVerify);
+      $("#verify").fadeIn();
+      setTimeout(function(){
+        $("#loader").fadeOut();
+      },1000);
+      $("#verify-btn").click(function() {
+        var verifyInput = $("#verify-input").val();
+        if (verifyInput == userCode) {
+          alert("holi");
+        };
+      });
+    });
   });
 
-  $("#phone-input").keyup(function() {
-    if ($(this).val().length >= 10) {
-      $("#signup-next-btn").removeClass("disabled");
-    } else {
-      $("#signup-next-btn").addClass("disabled");
-    }
-  })
 });
 
 // alert("It seems that we don't have your calling code. Please add it manually :)");
---
+++ assets/js/dom.js
@@ -0,0 +1,6 @@
+var sectionSignup = '<section id="signup" class="center-align"><div class="row"><div class="col s12"><h2>Sign Up</h2></div></div>'+
+'<div class="row"><div class="col s12"><h3>Get a ride in minutes</h3></div></div>'+
+'<div class="row"><div class="col s12 l6 xl6"><input type="text" id="country"><label for="">We will send a text to verify your phone</label></div>'+
+'<div class="col s12 l6 xl6"><input id="phone-input" type="text"></div></div><div class="row"><div class="col s12"><a id="signup-next-btn" class="waves-effect waves-light btn-large disabled">Next</a></div></div></section>';
+var sectionVerify = '<section id="verify" class="center-align"><div class="row"><div class="col s12"><h2>Verify Your Phone Number</h2>'+
+'<p>Join now for free ride credit!</p><div class="row"><div class="col s12"><input id="verify-input" type="text"></div></div><div class="row"><div class="col s12"><a id="verify-btn" class="waves-effect waves-light btn-large">Next</a></div></div></div></div></section>';
