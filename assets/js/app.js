ee240f81b4bbd416daf57a4ec746ac9e8ecb5e70
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-14 02:41:22 -0300

    Data added to index and app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
   $("#country").countrySelect();
-  $.getJSON("https://github.com/mledoze/countries/blob/master/countries.json", function() {
-    console.log(this);
-  })
   setTimeout(function(){
     $("#loader").fadeOut();
   },5000);
