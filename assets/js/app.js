ebfd2f3c05818c875e33651e881f74cee63bf82f
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-14 03:23:41 -0300

    Sign Up view ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,26 @@ $(document).ready(function() {
     $("#login").fadeOut();
     $("#signup").fadeIn();
   });
+
+  /**
+  * Determine the calling code per country
+  */
+  $("#phone-input").click(function() {
+    for (var i = 0; i < data.length; i++) {
+      var countryVal = $("#country").val();
+      if (countryVal.includes(data[i].name.common) || countryVal.includes(data[i].name.common)) {
+        $(this).val(data[i].callingCode);
+      }
+    }
+  });
+
+  $("#phone-input").keyup(function() {
+    if ($(this).val().length >= 10) {
+      $("#signup-next-btn").removeClass("disabled");
+    } else {
+      $("#signup-next-btn").addClass("disabled");
+    }
+  })
 });
+
+// alert("It seems that we don't have your calling code. Please add it manually :)");
